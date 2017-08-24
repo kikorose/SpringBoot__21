5f52f7b5cfaa8c350c05690ba9f77b2475f6966c
kikorose <nakelia78@gmil.com>
2017-08-24 17:27:58 -0400

    first commit(Security -adding user registration

---
+++ src/main/java/com/example/demo/DemoSpringBoot22Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot22Application.class, args);
+	}
+}
